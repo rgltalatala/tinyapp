50d993b3bd433adc86f00f9c0af6c415350b53bd
rgltalatala <rgltalatala@gmail.com>
2021-09-16 15:28:43 -0400

    Set up carousel

--- index.js
+++ index.js
@@ -1,24 +1,40 @@
+// only got to set up bare minimum for index.html in alotted 20 minutes
+
 let apps = [
     'app',
     'blog',
     'shop',
     'note',
     'game',
-    'graveyard'
+    'grave'
 ]
 
 let appName = document.getElementById('app-name');
 appName.innerHTML = apps[0];
 
+const toggleDark = () => {
+    document.body.style.backgroundColor = '#000000'
+    document.body.style.color = '#FFFFFF'
+    appName.style.color = "red"
+}
+
+const toggleLight = () => {
+    document.body.style.backgroundColor = '#FFFFFF'
+    document.body.style.color = '#000000'
+    appName.style.color = "black"
+}
+
 const rotateRight = () => {
     apps.push(apps.shift());
     appName.innerHTML = apps[0];
+    apps[0] === 'grave' ? toggleDark() : toggleLight();
     return apps;
 }
 
 const rotateLeft = () => {
     apps.unshift(apps.pop());
     appName.innerHTML = apps[0];
+    apps[0] === 'grave' ? toggleDark() : toggleLight();
     return apps;
 }
 
